2778c2ef50d0d0c1b7331770d8838ba1b821ed1b
Mathias Feitzinger <mathias@feitzinger.org>
2018-05-04 19:19:21 +0200

    Command Setup

---
+++ TravelTalkBackend/CQRS/Commands/GeneralCommandEvents/CommandHandled.cs
@@ -0,0 +1,42 @@
+﻿namespace Commands.GeneralCommandEvents {
+    using GeneralCommandHandling;
+
+    public class CommandHandled<TCommand, TCommandResult>
+            where TCommand : ICommand<TCommandResult>
+            where TCommandResult : ICommandResult {
+
+        private CommandHandled(TCommand command, TCommandResult result, CommandResultStatus resultStatus, string commandProcessingMessage) {
+            Command = command;
+            Result = result;
+            ResultStatus = resultStatus;
+            CommandProcessingMessage = commandProcessingMessage;
+        }
+
+        public TCommand Command { get; }
+
+        public TCommandResult Result { get; }
+
+        public string CommandProcessingMessage { get; }
+
+        public CommandResultStatus ResultStatus { get; }
+
+        public static CommandHandled<TCommand, TCommandResult> CreateSuccessfulResult(TCommand query, TCommandResult result) {
+            return new CommandHandled<TCommand, TCommandResult>(query, result, CommandResultStatus.SUCCESSFUL, "Command was successful handled");
+        }
+
+        public static CommandHandled<TCommand, TCommandResult> CreateUnsuccessfulResult(TCommand command) {
+            return new CommandHandled<TCommand, TCommandResult>(command, default(TCommandResult), CommandResultStatus.GENERAL_ERROR,
+                    "Command not successful handled");
+        }
+
+        public static CommandHandled<TCommand, TCommandResult> CreateUnsuccessfulResult(TCommand command, CommandResultStatus resultStatus) {
+            return new CommandHandled<TCommand, TCommandResult>(command, default(TCommandResult), resultStatus, "Command not successful handled");
+        }
+
+        public static CommandHandled<TCommand, TCommandResult> CreateUnsuccessfulResult(TCommand command, CommandResultStatus resultStatus,
+                                                                                        string unsuccessfulMessage) {
+            return new CommandHandled<TCommand, TCommandResult>(command, default(TCommandResult), resultStatus, unsuccessfulMessage);
+        }
+    }
+
+}
---
+++ TravelTalkBackend/CQRS/Commands/GeneralCommandHandling/CommandWorkerActor.cs
@@ -0,0 +1,59 @@
+﻿namespace Commands.GeneralCommandHandling {
+    using System;
+    using Akka.Actor;
+    using GeneralCommandEvents;
+
+    internal sealed class CommandWorkerActor<TCommand, TCommandResult> : ReceiveActor
+            where TCommand : ICommand<TCommandResult>
+            where TCommandResult : ICommandResult {
+        private TCommand command;
+
+        private IActorRef commandSender;
+
+        public CommandWorkerActor() {
+
+            Receive<CommandWorkerJob>(x => {
+                commandSender = Sender;
+                command = x.Command;
+                Context.ActorOf(x.Props).Tell(command);
+                SetReceiveTimeout(TimeSpan.FromSeconds(20));
+
+                Become(WaitForAnswer);
+            });
+        }
+
+        private void WaitForAnswer() {
+            Receive<ReceiveTimeout>(x => {
+                commandSender.Tell(CommandHandled<TCommand, TCommandResult>.CreateUnsuccessfulResult(command));
+                Self.Tell(PoisonPill.Instance);
+            });
+
+            Receive<CommandNotSuccessful>(x => {
+                commandSender.Tell(CommandHandled<TCommand, TCommandResult>.CreateUnsuccessfulResult(command, x.CommandStatus));
+                Self.Tell(PoisonPill.Instance);
+            });
+
+            Receive<CommandHandled<TCommand, TCommandResult>>(x => {
+                commandSender.Tell(x);
+                Self.Tell(PoisonPill.Instance);
+            });
+
+            Receive<TCommandResult>(x => {
+                commandSender.Tell(CommandHandled<TCommand, TCommandResult>.CreateSuccessfulResult(command, x));
+                Self.Tell(PoisonPill.Instance);
+            });
+        }
+
+        public class CommandWorkerJob {
+
+            public CommandWorkerJob(Props props, TCommand command) {
+                Props = props;
+                Command = command;
+            }
+
+            public Props Props { get; }
+
+            public TCommand Command { get; }
+        }
+    }
+}
