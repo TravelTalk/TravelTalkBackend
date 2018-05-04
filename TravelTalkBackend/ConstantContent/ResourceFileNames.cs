5ef246ffe03290e0c52a5625be494bb28e1b279e
Mathias Feitzinger <mathias@feitzinger.org>
2018-05-04 21:02:09 +0200

    Lighthouse added

--- TravelTalkBackend/ConstantContent/ResourceFileNames.cs
+++ TravelTalkBackend/ConstantContent/ResourceFileNames.cs
@@ -4,5 +4,7 @@
 
         public const string COMMAND_SERVICE_CONFIG_FILE = "commandService.hocon";
         public const string WEP_API_CONFIG_FILE_NAME = "webApi.hocon";
+        public const string LIGHTHOUSE_CONFIG_NAME = "lighthouse.hocon";
+        
     }
 }
