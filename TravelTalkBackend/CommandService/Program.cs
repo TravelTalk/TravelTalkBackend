07af7ccdc25ce4b1a74404e8b3c88d46d39f0e13
Mathias Feitzinger <mathias@feitzinger.org>
2018-05-04 18:35:02 +0200

    Init CommandService

---
+++ TravelTalkBackend/CommandService/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace CommandService {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello Command Service!");
+            
+            
+            DomainHostingService domainHostingService = new DomainHostingService();
+            domainHostingService.Start();
+
+            Console.CancelKeyPress += (sender, eventArgs) => domainHostingService.StopAsync().GetAwaiter().GetResult();
+            domainHostingService.TerminationHandle.Wait();
+        }
+    }
+}
